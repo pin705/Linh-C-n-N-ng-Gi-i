d31b9f087dca86a6707f7d8b4cbed9f21f70980f
pin705 <pinnguyen0597@gmail.com>
2025-10-17 09:15:30 +0700

    Refactor User model to include additional fields: lastKnownIP, userAgent, and isBanned

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,10 +1,16 @@
-// file: server/models/user.model.ts
 import { defineMongooseModel } from '#nuxt/mongoose'
 
-export const User = defineMongooseModel('User', {
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Luôn nhớ hash mật khẩu này!
-}, {
-  timestamps: true
-})
+export const User = defineMongooseModel(
+  'User',
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+
+    lastKnownIP: { type: String, default: '' },
+    userAgent: { type: String, default: '' },
+    isBanned: { type: Boolean, default: false }
+  },
+  {
+    timestamps: true
+  }
+)
